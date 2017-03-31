c1a442dea24f611c1b3413380790e945a45732db
artistbuddy <artist.buddy@gmail.com>
2017-03-31 12:08:47 +0200

    Boolean extension
    return opposite value

---
+++ customStack/Bool.swift
@@ -0,0 +1,15 @@
+//
+//  Bool.swift
+//  customStack
+//
+//  Created by Karol on 30/03/2017.
+//  Copyright © 2017 Karol Bukowski. All rights reserved.
+//
+
+import Foundation
+
+extension Bool {
+    func getOpposite() -> Bool {
+        return self ? false : true
+    }
+}
